d01b3d409c2b85cdc4da4abfaa98bba2696669dd
hanpu <hanpu.mwx@gmail.com>
2021-01-03 22:57:41 +0800

    disable geiser auto-complete as very slow

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,6 +1,8 @@
-(add-hook 'scheme-mode
-	  (lambda ()
-	    (setq company-backends
-		  (delete 'geiser-company-backend company-backends))))
+(use-package geiser
+  :config
+  (setq geiser-active-implementations '(mit)
+	;; disable auto-complete as very slow
+	geiser-repl-company-p nil
+	geiser-mode-company-p nil))
 
 (provide 'init-lisp)
